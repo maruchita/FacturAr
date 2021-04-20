93efc082f044c3ee9dcb86cd15b21a08c18d8f07
TomasAngeleri <tomi.angeleri94@gmail.com>
2021-04-20 17:12:47 -0300

    Primer commit

---
+++ src/main/java/com/facturar/app/FacturArApplication.java
@@ -0,0 +1,13 @@
+package com.facturar.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturArApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturArApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facturar/app/FacturArApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facturar.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturArApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
