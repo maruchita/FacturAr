077040c684ce4b568554d4d4cdf725815f0b838e
TomasAngeleri <tomi.angeleri94@gmail.com>
2021-04-22 22:06:33 -0300

    Primer servicio funcional

---
+++ src/main/java/com/facturar/app/entity/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.facturar.app.entity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.facturar.app.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
